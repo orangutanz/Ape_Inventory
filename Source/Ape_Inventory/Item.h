7d6fbc1eee53be86054d04b8c11e388a83b7f367
YuHan Ma <59942793+orangutanz@users.noreply.github.com>
2021-08-11 16:46:17 -0700

    Item implementation_1

--- Source/Ape_Inventory/Item.h
+++ Source/Ape_Inventory/Item.h
@@ -6,12 +6,51 @@
 #include "UObject/NoExportTypes.h"
 #include "Item.generated.h"
 
+UENUM(BlueprintType)
+enum ItemType
+{
+	Equipment        UMETA(DisplayName = "Equipment"),
+	Consumable       UMETA(DisplayName = "Consumable"),
+	MaterialItem     UMETA(DisplayName = "MaterialItem"),
+	Container        UMETA(DisplayName = "Container"),
+	Miscellaneous    UMETA(DisplayName = "Miscellaneous")
+};
 /**
  * 
  */
-UCLASS()
+UCLASS(Abstract, BlueprintType, Blueprintable, EditInlineNew, DefaultToInstanced)
 class APE_INVENTORY_API UItem : public UObject
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Item")
+	FName itemID;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Item")
+	FText Name;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Item", meta = (MultiLine = true))
+	FText Description;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Item")
+	TEnumAsByte<ItemType> itemType;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Item")
+	class UTexture2D* Icon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Item", meta = (ClampMin = 0))
+	int maxStack = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Item", meta = (ClampMin = 0))
+	int Quantity = 0;
+
+	UPROPERTY()
+	class UInventoryComponent* OwnerInventory;
+
+	virtual void Use(class AApe_InventoryCharacter* Character) PURE_VIRTUAL(UItem, );
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnUse(class AApe_InventoryCharacter* Character);
 };
