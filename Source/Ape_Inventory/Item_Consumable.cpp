011b989f5509b9e85a66470926a91c90e7146b62
YuHan Ma <59942793+orangutanz@users.noreply.github.com>
2021-08-13 11:17:38 -0700

    Adding condition for use consumable

--- Source/Ape_Inventory/Item_Consumable.cpp
+++ Source/Ape_Inventory/Item_Consumable.cpp
@@ -6,9 +6,12 @@ void UItem_Consumable::Use(AApe_InventoryCharacter* character)
 {
 	if (character)
 	{
-		character->health += regenAmount;
-		Quantity--;
-		if (Quantity == 0)
+		if (Quantity > 0)
+		{
+			character->health += regenAmount;
+			Quantity--;
+		}
+		if (OwnerInventory && Quantity == 0)
 		{
 			OwnerInventory->RemoveItem(this);
 		}
